035f2d28cdef64b5949558d4e370fb70de5005f5
GoodDeveloper2018 <80232792+GoodDeveloper2018@users.noreply.github.com>
2024-05-14 09:04:27 -0400

    Update mainMenu.py

--- mainMenu.py
+++ mainMenu.py
@@ -3,14 +3,24 @@ from pygame.locals import *
 
 
 pygame.init()
-screen = pygame.display.set_mode((1024, 768))
-while True:
-    screen.fill((0,0,0))
-    for event in pygame.event.get():
-                if not hasattr(event, 'key'): continue
-                if event.key == K_SPACE:
-                    main.level1()
-                elif event.key == K_ESCAPE: sys.exit(0)
-    img = pygame.image.load("images/main_menu_image.png")
-    screen.blit(img,(0,0))
-    pygame.display.flip()
+def game_intro():
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        screen.fill("white")
+        font = pygame.font.Font('freesansbold.ttf', 32)
+
+        intro_text = font.render("Cannibal Cars", True, (0, 0, 0))
+        controls_text = font.render("Arrow keys to move", True, (0, 0, 0))
+        controls_text2 = font.render("Space bar to transform", True, (0, 0, 0))
+        controls_text3 = font.render("Stop pressing any key to return to normal", True, (0, 0, 0))
+        screen.blit(intro_text, (300, 100))
+        screen.blit(controls_text, (20, 150))
+        screen.blit(controls_text2, (20, 200))
+        screen.blit(controls_text3, (20, 250))
+        button("Play", 300, 450, 100, 50, green, bright_green, "play")
+        pygame.display.update()
+        clock.tick(15)
